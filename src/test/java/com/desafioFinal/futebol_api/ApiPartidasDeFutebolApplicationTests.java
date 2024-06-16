6a9724dcdaa006dd8bdd8a16198fea04b8311d81
GuilhermeTeotonio <guilhermeteotonio.contato@gmail.com>
2024-06-16 17:12:34 -0300

    Initial commit

---
+++ src/test/java/com/desafioFinal/futebol_api/ApiPartidasDeFutebolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioFinal.futebol_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPartidasDeFutebolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
